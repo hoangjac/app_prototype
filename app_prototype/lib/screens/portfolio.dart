2f64a2fd02f64befc7b6179dbb7a34cb021819ae
Jack Hoang <hoangjac@sheridancollege.ca>
2021-03-28 20:32:57 -0400

    added photos to cells

--- app_prototype/lib/screens/portfolio.dart
+++ app_prototype/lib/screens/portfolio.dart
@@ -11,6 +11,9 @@ class _PortfolioState extends State<Portfolio> {
     return DataTable(
       columns: const <DataColumn>[
         DataColumn(
+          label: Text(""),
+        ),
+        DataColumn(
           label: Text(
             "Card",
             style: TextStyle(fontStyle: FontStyle.italic),
@@ -29,9 +32,10 @@ class _PortfolioState extends State<Portfolio> {
           ),
         ),
       ],
-      rows: const <DataRow>[
+      rows: <DataRow>[
         DataRow(
           cells: <DataCell>[
+            DataCell(Image.asset('images/pikachu.JPG')),
             DataCell(Text('Pikachu')),
             DataCell(Text('10')),
             DataCell(Text('\$200')),
@@ -39,6 +43,10 @@ class _PortfolioState extends State<Portfolio> {
         ),
         DataRow(
           cells: <DataCell>[
+            DataCell(Image.asset(
+              'images/charizard.jpeg',
+              width: 35,
+            )),
             DataCell(Text('Charizard')),
             DataCell(Text('9')),
             DataCell(Text('\$10000')),
@@ -46,6 +54,7 @@ class _PortfolioState extends State<Portfolio> {
         ),
         DataRow(
           cells: <DataCell>[
+            DataCell(Image.asset('images/lugia.jpg')),
             DataCell(Text('Lugia')),
             DataCell(Text('10')),
             DataCell(Text('\$5000')),
